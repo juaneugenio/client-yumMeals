3d91a94c49fe1758f85265bdd4897c02d116e5b7
Juan Eugenio <leuviah77@gmail.com>
2021-11-23 14:14:46 +0100

    Changed Appname in navbar and indexhtml

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -5,35 +7,35 @@ import * as PATHS from "../../utils/paths";
 import * as CONSTS from "../../utils/consts";
 
 const Navbar = (props) => {
-  return (
-    <nav>
-      <Link to={PATHS.HOMEPAGE} className="nav__projectName">
-        {CONSTS.CAPITALIZED_APP} - created with IronLauncher
-      </Link>
+	return (
+		<nav>
+			<Link to={PATHS.HOMEPAGE} className="nav__projectName">
+				{CONSTS.CAPITALIZED_APP}
+			</Link>
 
-      <div className="nav__authLinks">
-        {props.user ? (
-          <>
-            <Link to={PATHS.PROTECTEDPAGE} className="authLink">
-              Protected Page
-            </Link>
-            <button className="nav-logoutbtn" onClick={props.handleLogout}>
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <Link to={PATHS.SIGNUPPAGE} className="authLink">
-              Signup
-            </Link>
-            <Link to={PATHS.LOGINPAGE} className="authLink">
-              Log In
-            </Link>
-          </>
-        )}
-      </div>
-    </nav>
-  );
+			<div className="nav__authLinks">
+				{props.user ? (
+					<>
+						<Link to={PATHS.PROTECTEDPAGE} className="authLink">
+							Protected Page
+						</Link>
+						<button className="nav-logoutbtn" onClick={props.handleLogout}>
+							Logout
+						</button>
+					</>
+				) : (
+					<>
+						<Link to={PATHS.SIGNUPPAGE} className="authLink">
+							Signup
+						</Link>
+						<Link to={PATHS.LOGINPAGE} className="authLink">
+							Log In
+						</Link>
+					</>
+				)}
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
