890952e0f5972290bc6a9b0a2f4ac5788f92b6ec
MFLuce <mfluce@yahoo.fr>
2021-11-25 20:37:32 +0100

    I don't remember what I did

--- src/utils/paths.js
+++ src/utils/paths.js
@@ -2,15 +2,15 @@
 export const HOME_PAGE = "/";
 export const SIGNUP_PAGE = "/auth/signup";
 export const LOGIN_PAGE = "/auth/login";
-export const SEARCH_RESULT_PAGE = "/result";
-export const SINGLE_RECIPE_PAGE = "/recipe/:id";
+export const SEARCH_RESULT_PAGE = "/result"; // MFL create this path
+export const SINGLE_RECIPE_PAGE = "/recipe/:id"; // MFL create this path
 
-// PROTECTED PAGES
+// PROTECTED PAGES created by MFL
 export const ACCOUNT_PAGE = "/account";
 export const EDIT_ACCOUNT_PAGE = "/account/edit";
-export const CARNET_PAGE = "/carnet"; // THE USER CAN VIEW HIS FAVORITES RECIPES AND HIS CONTRIBUTIONS
-export const FAVORITES_PAGE = "/carnet/favorites";
-export const CONTRIBUTIONS_PAGE = "/carnet/contributions"; // it's where you see all the recipes you create
+export const DASHBOARD_PAGE = "/dashboard"; // THE USER CAN VIEW HIS FAVORITES RECIPES AND HIS CONTRIBUTIONS
+export const FAVORITES_PAGE = "/dashboard/favorites";
+export const CREATIONS_PAGE = "/dashboard/creations"; // it's where you see all the recipes you create
 export const CREATE_RECIPE_PAGE = "/recipe/create";
 export const EDIT_RECIPE_PAGE = "/recipe/edit"; // edit and delete the recipe
 export const RATINGS_COMMENT_PAGE = "/recipe/comment";
