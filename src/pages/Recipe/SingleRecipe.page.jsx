20f899cd8033f88c04893e29e8ec16e8abfeaeb6
Anna Velarde <annavelardevilaseca@gmail.com>
2021-12-09 20:24:24 +0000

    updating

--- src/pages/Recipe/SingleRecipe.page.jsx
+++ src/pages/Recipe/SingleRecipe.page.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, Container, Form, Button} from "react-bootstrap";
+import { Card, Container, Form, Button } from "react-bootstrap";
 import { useParams, useNavigate } from "react-router";
 import * as PATHS from "../../utils/paths";
 import {
@@ -16,9 +16,9 @@ function SingleRecipe() {
   const [singleRecipe, setSingleRecipe] = useState(undefined);
   console.log("singleRecipe1:", singleRecipe);
   const [error, setError] = useState("");
-  const [Loading, setLoading] = useState(true);  
+  const [Loading, setLoading] = useState(true);
 
-    useEffect(() => {
+  useEffect(() => {
     setLoading(true);
     getSingleRecipe(recipeId)
       .then((recipe) => {
@@ -43,34 +43,32 @@ function SingleRecipe() {
   }
 
   //CREATE THE RATING COMPONENT
-//FIRST THE CONST
+  //FIRST THE CONST
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
   const [form, setForm] = useState({
     userRating: "",
     comment: "",
   });
-  const { userRating, comment, } = form;
+  const { userRating, comment } = form;
 
-  //IN THE SUBMIT EVENT WE PUT THE RATING FUNCTION  
+  //IN THE SUBMIT EVENT WE PUT THE RATING FUNCTION
   function handleSubmit(event) {
     event.preventDefault();
     setLoading(true);
     setError(false);
-//THIS FUNCTION COMES FROM THE RECIPE SERVICE
-  createRating({ userRating, comment, recipeId}).then(
-    (res) => {
+    //THIS FUNCTION COMES FROM THE RECIPE SERVICE
+    createRating({ userRating, comment, recipeId }).then((res) => {
       console.log("RES:", res);
       if (!res.success) {
         return setError(res.data);
       }
       navigate(PATHS.HOME_PAGE);
-    }
-  );
+    });
   }
 
-   // It comes from RecipeService
-   function handleDeleteSingleRecipe() {
+  // It comes from RecipeService
+  function handleDeleteSingleRecipe() {
     setLoading(true);
     deleteSingleRecipe(recipeId)
       .then((response) => {
@@ -83,10 +81,10 @@ function SingleRecipe() {
         setError(message);
       })
       .finally(() => {
-       setLoading(false);
-       });
+        setLoading(false);
+      });
   }
-  
+
   if (Loading) {
     return <div>Loading...</div>;
   }
@@ -109,53 +107,53 @@ function SingleRecipe() {
             ))}
           </ol>
         </Card.Body>
-      <button onClick={handleDeleteSingleRecipe} type="delete">
-        Delete Recipe
-      </button>
+        <button onClick={handleDeleteSingleRecipe} type="delete">
+          Delete Recipe
+        </button>
 
-      <Form onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>Please rate this recipe if you already did it !</legend>
-          <Form.Group>
-            {[...Array(5)].map((star, i) => {
-              const ratingValue = i + 1;
-              return (
-                <Form.Label>
-                  <input
-                    type="radio"
-                    name="userRating"
-                    value={ratingValue}
-                    onClick={() => setRating(ratingValue)}
-                    onChange={handleNormalInput}
-                  />
-                  <FaStar
-                    className="star"
-                    color={
-                      ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"
-                    }
-                    onMouseEnter={() => setHover(ratingValue)}
-                    onMouseLeave={() => setHover(null)}
-                    size={20}
-                  />
-                </Form.Label>
-              );
-            })}
-          </Form.Group>
-          
-          <Form.Group>
-          <Form.Control 
-          as="textarea" 
-          rows={3} 
-          type="text" 
-          name="comment" 
-          value={comment} 
-          onChange={handleNormalInput} 
-          placeholder="Write your comment about the recipe"/>
+        <Form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Please rate this recipe if you already did it !</legend>
+            <Form.Group>
+              {[...Array(5)].map((star, i) => {
+                const ratingValue = i + 1;
+                return (
+                  <Form.Label>
+                    <input
+                      type="radio"
+                      name="userRating"
+                      value={ratingValue}
+                      onClick={() => setRating(ratingValue)}
+                      onChange={handleNormalInput}
+                    />
+                    <FaStar
+                      className="star"
+                      color={
+                        ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"
+                      }
+                      onMouseEnter={() => setHover(ratingValue)}
+                      onMouseLeave={() => setHover(null)}
+                      size={20}
+                    />
+                  </Form.Label>
+                );
+              })}
+            </Form.Group>
 
-          </Form.Group>
-          <Button type="submit">Submit</Button>
-        </fieldset>
-      </Form>
+            <Form.Group>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                type="text"
+                name="comment"
+                value={comment}
+                onChange={handleNormalInput}
+                placeholder="Write your comment about the recipe"
+              />
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+          </fieldset>
+        </Form>
       </Card>
     </Container>
   );
