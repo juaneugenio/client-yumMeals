fe95dcb00e8fbfa0081131568dfd3db500031881
juaneugenio <leuviah77@gmail.com>
2021-12-11 18:06:07 +0100

    merge dev & fix conflicts. App functional

--- src/pages/Recipe/SingleRecipe.page.jsx
+++ src/pages/Recipe/SingleRecipe.page.jsx
@@ -92,88 +92,88 @@ function SingleRecipe({ user }) {
         }
         navigate(PATHS.HOME_PAGE);
       });
-
-    if (isLoading) {
-      return <LoadingComponent />;
-    }
-
-    if (error) {
-      return <div>{error}</div>;
-    }
-
-    return (
-      <Container>
-        <Card>
-          <Card.Body>
-            <Card.Text className="h1">{singleRecipe.title}</Card.Text>
-            <Card.Subtitle>by {singleRecipe.owner.username}</Card.Subtitle>
-            <Card.Text>Category: {singleRecipe.category}</Card.Text>
-            <Card.Text className="h3">Steps of the recipe:</Card.Text>
-            <ol className="list-group list-group-numbered">
-              {singleRecipe.ingredients.map((step) => (
-                <li className="list-group-item">{step}</li>
-              ))}
-            </ol>
-          </Card.Body>
-
-          {isOwner() && (
-            <div className="d-flex mt-3 gap-2">
-              <Link key={singleRecipe._id} to={`/recipe/edit/${recipeId}`}>
-                <Button variant="primary">Edit Recipe</Button>
-              </Link>
-              <Button variant="danger" onClick={handleDeleteSingleRecipe} type="delete">
-                Delete Recipe
-              </Button>
-            </div>
-          )}
-
-          <Form onSubmit={handleSubmit}>
-            <fieldset>
-              <legend>Please rate this recipe if you already did it !</legend>
-              <Form.Group>
-                {[...Array(5)].map((star, i) => {
-                  const ratingValue = i + 1;
-                  return (
-                    <Form.Label>
-                      <input
-                        type="radio"
-                        name="userRating"
-                        value={ratingValue}
-                        onClick={() => setRating(ratingValue)}
-                        onChange={handleNormalInput}
-                      />
-                      <FaStar
-                        className="star"
-                        color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
-                        onMouseEnter={() => setHover(ratingValue)}
-                        onMouseLeave={() => setHover(null)}
-                        size={20}
-                      />
-                    </Form.Label>
-                  );
-                })}
-              </Form.Group>
-
-              <Form.Group>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  type="text"
-                  name="comment"
-                  value={comment}
-                  onChange={handleNormalInput}
-                  placeholder="Write your comment about the recipe"
-                />
-              </Form.Group>
-              <Button type="submit">Submit</Button>
-            </fieldset>
-          </Form>
-        </Card>
-
-        {isOwner() && <EditRecipe recipe={singleRecipe} />}
-      </Container>
-    );
   }
+
+  if (isLoading) {
+    return <LoadingComponent />;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <Container>
+      <Card>
+        <Card.Body>
+          <Card.Text className="h1">{singleRecipe.title}</Card.Text>
+          <Card.Subtitle>by {singleRecipe.owner.username}</Card.Subtitle>
+          <Card.Text>Category: {singleRecipe.category}</Card.Text>
+          <Card.Text className="h3">Steps of the recipe:</Card.Text>
+          <ol className="list-group list-group-numbered">
+            {singleRecipe.ingredients.map((step) => (
+              <li className="list-group-item">{step}</li>
+            ))}
+          </ol>
+        </Card.Body>
+
+        {isOwner() && (
+          <div className="d-flex mt-3 gap-2">
+            <Link key={singleRecipe._id} to={`/recipe/edit/${recipeId}`}>
+              <Button variant="primary">Edit Recipe</Button>
+            </Link>
+            <Button variant="danger" onClick={handleDeleteSingleRecipe} type="delete">
+              Delete Recipe
+            </Button>
+          </div>
+        )}
+
+        <Form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Please rate this recipe if you already did it !</legend>
+            <Form.Group>
+              {[...Array(5)].map((star, i) => {
+                const ratingValue = i + 1;
+                return (
+                  <Form.Label>
+                    <input
+                      type="radio"
+                      name="userRating"
+                      value={ratingValue}
+                      onClick={() => setRating(ratingValue)}
+                      onChange={handleNormalInput}
+                    />
+                    <FaStar
+                      className="star"
+                      color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                      onMouseEnter={() => setHover(ratingValue)}
+                      onMouseLeave={() => setHover(null)}
+                      size={20}
+                    />
+                  </Form.Label>
+                );
+              })}
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                type="text"
+                name="comment"
+                value={comment}
+                onChange={handleNormalInput}
+                placeholder="Write your comment about the recipe"
+              />
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+          </fieldset>
+        </Form>
+      </Card>
+
+      {isOwner() && <EditRecipe recipe={singleRecipe} />}
+    </Container>
+  );
 }
 
 export default SingleRecipe;
