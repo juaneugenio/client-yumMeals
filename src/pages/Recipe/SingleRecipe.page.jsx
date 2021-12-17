8f2520035423a1ec66434939c2a9ed8515b53c53
MFLuce <mfluce@yahoo.fr>
2021-12-17 15:22:35 +0100

    Merging with Juan

--- src/pages/Recipe/SingleRecipe.page.jsx
+++ src/pages/Recipe/SingleRecipe.page.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Card, Container, Button, Row, Col } from "react-bootstrap";
+import {
+  Card,
+  Container,
+  Button,
+  ListGroup,
+  ListGroupItem,
+} from "react-bootstrap";
 import { useParams, useNavigate } from "react-router";
 import * as PATHS from "../../utils/paths";
-import { getSingleRecipe, deleteSingleRecipe } from "../../services/recipeService";
+import {
+  getSingleRecipe,
+  deleteSingleRecipe,
+} from "../../services/recipeService";
 import "../Recipe/SingleRecipePage.css";
 import { Link } from "react-router-dom";
 import EditRecipe from "../../components/EditRecipe";
@@ -14,7 +23,7 @@ function SingleRecipe({ user }) {
   console.log("user-----> ", user);
   const { recipeId } = useParams();
   const [singleRecipe, setSingleRecipe] = useState(undefined);
-  //console.log("singleRecipe CONSOLE:", singleRecipe);
+  // console.log("singleRecipe1:", singleRecipe);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
@@ -34,7 +43,6 @@ function SingleRecipe({ user }) {
     setIsLoading(true);
     getSingleRecipe(recipeId)
       .then((recipe) => {
-        console.log("MIRAAAAAAA", { recipe });
         console.log("///***recIsRated:", recIsRated());
         console.log("///***isRated:", isRated());
         console.log("recipeId:", recipeId);
@@ -48,7 +56,10 @@ function SingleRecipe({ user }) {
 
         console.log("*****recipe.data.recipe:", recipe.data.recipe);
         console.log("*****recipe.data.rating:", recipe.data.rating);
-        console.log("*****recipe.data.recipeIsRated:", recipe.data.recipeIsRated);
+        console.log(
+          "*****recipe.data.recipeIsRated:",
+          recipe.data.recipeIsRated
+        );
         console.log("///***recIsRated:", recIsRated());
         console.log("///***isRated:", isRated());
       })
@@ -94,84 +105,88 @@ function SingleRecipe({ user }) {
 
   return (
     <Container className="mt-2 p-5">
-      <Row>
-        <Col sm={8}>
-          <Card.Img
-            className="card-image"
+      <Card>
+        <Card.Body>
+          <img
             height={"500px"}
             src={singleRecipe.imageRecipe}
             alt={`${singleRecipe.title}'s meal`}
           />
-        </Col>
-        <Col>
-          <Card.Body>
-            <Card.Text className="h1 mt-0 text-uppercase">{singleRecipe.title}</Card.Text>
-            <Card.Subtitle className="mx-4 pt-1 text-secondary blockquote-footer">
-              by {singleRecipe.owner.username}
-            </Card.Subtitle>
-            <Card.Text className="h6 mt-4">
-              <b>Category:</b>
-            </Card.Text>
-            <Card.Subtitle className="text-secondary">
-              {singleRecipe.category}
-            </Card.Subtitle>
-            <Card.Text className="h6 mt-4">
-              <b>Cooking time </b>🕒:
-            </Card.Text>
-            <Card.Subtitle className="text-secondary">
-              {singleRecipe.cookingTime}
-            </Card.Subtitle>
-            <Card.Text className="h6 mt-4">
-              <b>Ingredients:</b>
-            </Card.Text>
-            <Card.Subtitle>
-              <ol className="list-group list-group-numbered">
-                {singleRecipe.ingredients.map((ingredient) => (
-                  <li className="list-group-item  text-secondary">{ingredient}</li>
-                ))}
-              </ol>
-            </Card.Subtitle>
-            <Card.Text className="h6 mt-4">
-              <b>Steps to follow:</b>
-            </Card.Text>
+          <Card.Text className="h1 mt-0 text-uppercase">
+            {singleRecipe.title}
+          </Card.Text>
+          <Card.Subtitle className="mx-4 pt-1 text-secondary blockquote-footer">
+            by {singleRecipe.owner.username}
+          </Card.Subtitle>
+          <Card.Text className="h6 mt-4">
+            <b>Category:</b>
+          </Card.Text>
+          <Card.Subtitle className="text-secondary">
+            {singleRecipe.category}
+          </Card.Subtitle>
+          <Card.Text className="h6 mt-4">
+            <b>Cooking time </b>🕒:
+          </Card.Text>
+          <Card.Subtitle className="text-secondary">
+            {singleRecipe.cookingTime}
+          </Card.Subtitle>
+          <Card.Text className="h6 mt-4">
+            <b>Ingredients:</b>
+          </Card.Text>
+          <Card.Subtitle>
             <ol className="list-group list-group-numbered">
-              {singleRecipe.stepsRecipe.map((step) => (
-                <li className="list-group-item  text-secondary">{step}</li>
+              {singleRecipe.ingredients.map((ingredient) => (
+                <li className="list-group-item  text-secondary">
+                  {ingredient}
+                </li>
               ))}
             </ol>
-            <Card.Text className="h6 mt-4">
-              <b>Rating:</b>
-            </Card.Text>
+          </Card.Subtitle>
+          <Card.Text className="h6 mt-4">
+            <b>Steps to follow:</b>
+          </Card.Text>
+          <ol className="list-group list-group-numbered">
+            {singleRecipe.stepsRecipe.map((step) => (
+              <li className="list-group-item  text-secondary">{step}</li>
+            ))}
+          </ol>
+          <Card.Text className="h6 mt-4">
+            <b>Rating:</b>
+          </Card.Text>
+        </Card.Body>
 
-            {isOwner() && (
-              <div className="d-flex mt-5 gap-3">
-                <Link key={singleRecipe._id} to={`/recipe/edit/${recipeId}`}>
-                  <Button variant="secondary">Edit Recipe</Button>
-                </Link>
-                <Button variant="danger" onClick={handleDeleteSingleRecipe} type="delete">
-                  Delete Recipe
-                </Button>
-              </div>
-            )}
-            {isOwner() && <EditRecipe recipe={singleRecipe} />}
-          </Card.Body>
-        </Col>
-      </Row>
+        {/* ///////////////////////////////////CREATE RATING/////////////////////////////////////////////// */}
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            {!user ||
+              (isNotOwner() && (
+                <p>
+                  <strong>Rate & Comment ! Log in or Sign Up !!!</strong>
+                </p>
+              ))}
+          </ListGroup.Item>
 
-      {/* ///////////////////////////////////CREATE RATING/////////////////////////////////////////////// */}
-      {!user ||
-        (isNotOwner() && (
-          <p>
-            <strong>Rate & Comment ! Log in or Sign Up !!!</strong>
-          </p>
-        ))}
-      {isLoggedIn() ||
-        !isRated() ||
-        (isNotOwner() && <RatingRecipe recipe={singleRecipe} />)}
+          <RatingRecipe recipe={singleRecipe} />
 
-      {isRated() || (isOwner() && <DisplayUserRating recipe={singleRecipe} />)}
-      {/* ///////////////////////////////////DISPLAY ALL RATINGS/////////////////////////////////////////////// */}
-      <DisplayRatings ratings={allRatings} />
+          {isRated() ||
+            (isOwner() && <DisplayUserRating recipe={singleRecipe} />)}
+          {/* ///////////////////////////////////DISPLAY ALL RATINGS/////////////////////////////////////////////// */}
+          <DisplayRatings ratings={allRatings} />
+        </ListGroup>
+      </Card>
+
+      {isOwner() && (
+        <div>
+          <EditRecipe recipe={singleRecipe} />
+          <Button
+            variant="danger"
+            onClick={handleDeleteSingleRecipe}
+            type="delete"
+          >
+            Delete Recipe
+          </Button>
+        </div>
+      )}
     </Container>
   );
 }
